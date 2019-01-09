5ba63ce5c89bfa3f2788c21787b5b19947fd48a9
mpxy <mpxy03@gmail.com>
2019-01-09 10:40:36 +0200

    Created basic layout and index page

---
+++ components/Layout.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.header``;
+const Main = styled.main``;
+const Footer = styled.footer``;
+
+export default ({ children, title = "Default title" }) => (
+  <Wrapper>
+    <Head>
+      <title>{title}</title>
+    </Head>
+    <Header>Header</Header>
+    <Main>{children}</Main>
+    <Footer>Footer</Footer>
+  </Wrapper>
+);
