53c6596f174038defa4d698fae147df3f4256d6c
mpxy <mpxy03@gmail.com>
2019-01-10 10:24:01 +0200

    Created express app

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const next = require("next");
+const bodyParser = require("body-parser");
+
+const port = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const routes = require("./routes.js");
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+    server.use(bodyParser.json());
+    server.use(bodyParser.urlencoded({ extended: true }));
+
+    server.use("/api", routes);
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(3000, err => {
+      if (err) throw err;
+      console.log(`Ready on http://localhost:${port}`);
+    });
+  })
+  .catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
