16edf814307ba1a1dbaf45d80d9393f87174846e
mpxy <mpxy03@gmail.com>
2019-01-16 11:35:25 +0200

    Created UserList component

---
+++ components/UserList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Api from "../common/api";
+
+class User extends React.Component {
+  deleteUser = () => {
+    Api.deleteUser(this.props.user.Username);
+  };
+
+  editUser = () => {
+    Api.editUser(this.props.Username);
+  };
+
+  render() {
+    console.log(this.props.user);
+    return (
+      <div>
+        <span>{this.props.user.Username}</span>
+        <button onClick={this.editUser}>Edit</button>
+        <button onClick={this.deleteUser}>Delete</button>
+      </div>
+    );
+  }
+}
+
+export default ({ users }) => (
+  <div>
+    <span>Users</span>
+    {users.map((user, key) => {
+      return <User key={key} user={user} />;
+    })}
+  </div>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,26 @@
+import React from "react";
+import "isomorphic-fetch";
 import Layout from "../components/Layout";
+import UserList from "../components/UserList";
+import Api from "../common/api";
 
-export default () => <Layout>Welcome to next.js!</Layout>;
+export default class extends React.PureComponent {
+  static async getInitialProps({ req }) {
+    return {
+      users: await Api.listUsers()
+    };
+  }
+
+  createUser() {
+    return Api.createUser();
+  }
+
+  render() {
+    return (
+      <Layout>
+        <button onClick={this.createUser}>Create User</button>
+        <UserList users={this.props.users} />
+      </Layout>
+    );
+  }
+}
