060f56b21c02067280190463ec3a60f2e73dd1e1
mpxy <mpxy03@gmail.com>
2019-01-22 13:39:28 +0200

    Updated layout styles

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,34 @@
 import Head from "next/head";
 import styled from "styled-components";
+import Link from "next/link";
 
 const Wrapper = styled.div`
+  height: 100%;
   display: flex;
   flex-direction: column;
 `;
 
-const Header = styled.header``;
-const Main = styled.main``;
-const Footer = styled.footer``;
+const Header = styled.header`
+  padding: 0.5rem 1rem;
+`;
+const Main = styled.main`
+  flex: 1;
+  padding: 0.5rem 1rem;
+`;
+const Footer = styled.footer`
+  padding: 0.5rem 1rem;
+`;
 
 export default ({ children, title = "Cognito Control Panel" }) => (
   <Wrapper>
     <Head>
       <title>{title}</title>
     </Head>
-    <Header>Header</Header>
+    <Header>
+      <Link href={"/"}>
+        <a>Home</a>
+      </Link>
+    </Header>
     <Main>{children}</Main>
     <Footer>Footer</Footer>
   </Wrapper>
--- styles/global-styles.js
+++ styles/global-styles.js
@@ -2,11 +2,21 @@ import { createGlobalStyle } from "styled-components";
 import { Colors } from "./theme";
 
 export default createGlobalStyle`
+  html {
+    height: 100%;
+  }
   body {
+    height: 100%;
     background-color: ${Colors.alabaster};
   }
-
   #__next {
     height: 100%;
+  }
+  .bp3-button {
+    outline: none;
+  }
+  .bp3-form-group label.bp3-label {
+    font-weight: 700;
+    text-transform: capitalize;
   }
 `;
