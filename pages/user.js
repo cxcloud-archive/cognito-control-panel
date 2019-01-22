0f92cc69e3422f59101d251ba15c5fb83d43cce6
mpxy <mpxy03@gmail.com>
2019-01-22 14:29:19 +0200

    Added buttons to user page

--- pages/user.js
+++ pages/user.js
@@ -6,7 +6,15 @@ import { FormGroup, InputGroup } from "@blueprintjs/core";
 import { Button, Intent } from "@blueprintjs/core";
 import DeleteUserDialog from "../components/DeleteUserDialog";
 import Head from "next/head";
+import styled from "styled-components";
 
+const formFields = ["name", "email", "custom:ssn"];
+
+const Buttons = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
 export default class extends React.Component {
   constructor(props) {
     super(props);
@@ -30,11 +38,16 @@ export default class extends React.Component {
   hideDeleteDialog = () => {
     this.setState({ isDeleteDialogOpen: false });
   };
-  deleteUser = () => {
-    Api.deleteUser(this.props.user.Username);
-  };
+  deleteUser = () => Api.deleteUser(this.props.user.Username);
   onChange = event => {
     console.log(event.target);
+  };
+
+  getAttributeValue = attributeName => {
+    const obj = this.state.user.UserAttributes.find(
+      item => item.Name === attributeName
+    );
+    return obj ? obj.Value : undefined;
   };
 
   render() {
@@ -49,23 +62,41 @@ export default class extends React.Component {
           onConfirm={this.deleteUser}
           onClose={this.hideDeleteDialog}
         />
-        <Button
-          icon='trash'
-          onClick={this.showDeleteDialog}
-          intent={Intent.DANGER}
-          minimal={false}
-        />
         <FormGroup label='Username'>{this.state.user.Username}</FormGroup>
-        {this.state.user.UserAttributes.map((item, key) => (
-          <FormGroup label={item.Name} labelFor={item.Name} key={key}>
+        {formFields.map((item, key) => (
+          <FormGroup
+            label={item}
+            labelFor={item}
+            key={key}
+            className='bp3-large'
+          >
             <input
               className='bp3-input bp3-large'
-              id={item.Name}
-              value={item.Value}
+              id={item}
+              value={this.getAttributeValue(item)}
               onChange={this.onChange}
             />
           </FormGroup>
         ))}
+        <Buttons>
+          <Button
+            icon='tick'
+            onClick={this.showDeleteDialog}
+            intent={Intent.PRIMARY}
+            minimal={false}
+            large={true}
+          >
+            Save
+          </Button>
+          <Button
+            icon='trash'
+            onClick={this.showDeleteDialog}
+            intent={Intent.DANGER}
+            minimal={true}
+          >
+            Delete
+          </Button>
+        </Buttons>
       </Layout>
     );
   }
