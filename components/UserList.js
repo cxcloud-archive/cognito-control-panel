37cf6a55f5ff8a54192cc74fcbfaf8a6539897ae
mpxy <mpxy03@gmail.com>
2019-01-24 16:20:08 +0200

    Refactored user list

--- components/UserList.js
+++ components/UserList.js
@@ -1,40 +1,30 @@
+import Link from "next/link";
 import React from "react";
 import ReactTable from "react-table";
-import Link from "next/link";
 
-const columnFields = ["Username", "name", "email", "custom:ssn"];
+const TABLE_COLUMNS = process.env.TABLE_COLUMNS.split(",");
 
-const columns = columnFields.map(column => {
-  if (column === "Username") {
-    return {
-      Header: "Username",
-      accessor: "Username",
-      Cell: ({ value }) => (
-        <Link href={`/user/${value}`}>
-          <a>{value}</a>
-        </Link>
-      )
-    };
-  } else {
-    return {
-      id: column,
-      Header: column,
-      // Header: column.replace("custom:", ""),
-      accessor: d => d.UserAttributes[column]
-    };
-  }
+const columns = TABLE_COLUMNS.map((columnName, key) => {
+  const column = {
+    id: columnName,
+    Header: columnName,
+    accessor: d =>
+      d[columnName] ? d[columnName] : d.UserAttributes[columnName]
+  };
+
+  // Add line in the first column
+  return key !== 0
+    ? column
+    : {
+        ...column,
+        Cell: ({ row, value }) => (
+          <Link href={`/user/${row.Username}`}>
+            <a>{value}</a>
+          </Link>
+        )
+      };
 });
 
-export default class extends React.Component {
-  render() {
-    return (
-      <div>
-        <ReactTable
-          data={this.props.users}
-          columns={columns}
-          className='-striped -highlight'
-        />
-      </div>
-    );
-  }
-}
+export default ({ users }) => (
+  <ReactTable data={users} columns={columns} className='-striped -highlight' />
+);
